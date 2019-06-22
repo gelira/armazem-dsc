56d565cea9616da047238b39c625de646ae72541
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-06-22 13:00:41 -0300

    fillable model Insumos

--- app/Insumo.php
+++ app/Insumo.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Insumo extends Model
 {
-    //
+    protected $fillable = [
+        'unidade_id', 'fornecedor_id', 'descricao', 'materia_prima', 'estoque'
+    ];
 }
