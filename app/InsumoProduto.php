a18c94711a1b06e83f287416062ce7a0c5f9db17
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-06-25 20:43:12 -0300

    fillable insumo produto

--- app/InsumoProduto.php
+++ app/InsumoProduto.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class InsumoProduto extends Pivot
 {
     protected $table = 'insumo_produto';
+
+    protected $fillable = ['produto_id', 'insumo_id', 'quantidade'];
 }
