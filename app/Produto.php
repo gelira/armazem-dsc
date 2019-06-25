d511e634136af3641ba2c00984106d0a6fbf6a0b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-06-25 18:32:18 -0300

    fillable e attributes Produtos

--- app/Produto.php
+++ app/Produto.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $fillable = [
+        'descricao', 'faixa_etaria_min', 'faixa_etaria_max', 'num_patente'
+    ];
+
+    protected $attributes = [
+        'estoque' => 0
+    ];
 }
