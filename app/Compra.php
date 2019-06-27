77f28570d630242b6fee1b029e0fd7e1495ddc9c
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-06-26 23:05:46 -0300

    fillable compras

--- app/Compra.php
+++ app/Compra.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Compra extends Model
 {
-    //
+    protected $fillable = [
+        'insumo_id', 'quantidade', 'valor_unitario', 'icms', 'frete', 
+        'ipi', 'nf', 'data_compra', 'data_entrega'
+    ];
 }
